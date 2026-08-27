234e7b5c3e11a2931387d46450c40df8551c543a
grnnfps1 <contatogrn@icloud.com>
2026-08-27 11:07:12 -0300

    Fix Java 17 List access in audit log test

--- src/test/java/com/computerstorage/common/transfer/TransferAuditLogTest.java
+++ src/test/java/com/computerstorage/common/transfer/TransferAuditLogTest.java
@@ -10,7 +10,7 @@ class TransferAuditLogTest {
         TransferAuditLog log = new TransferAuditLog();
         for (int i = 0; i < 105; i++) log.record(i, "p", i);
         assertEquals(100, log.entries().size());
-        assertEquals(5, log.entries().getFirst().gameTime());
-        assertEquals(104, log.entries().getLast().gameTime());
+        assertEquals(5, log.entries().get(0).gameTime());
+        assertEquals(104, log.entries().get(log.entries().size() - 1).gameTime());
     }
 }
