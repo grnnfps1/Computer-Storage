09a13cb209a492b54088510df7c1e8144f88d6c0
grnnfps1 <contatogrn@icloud.com>
2026-08-27 00:10:01 -0300

    feat(registry): bootstrap block and menu registries

--- src/main/java/com/computerstorage/ComputerStorage.java
+++ src/main/java/com/computerstorage/ComputerStorage.java
@@ -1,9 +1,14 @@
 package com.computerstorage;
 
 import com.computerstorage.common.computer.Computer;
+import com.computerstorage.common.registry.ModBlockEntities;
+import com.computerstorage.common.registry.ModBlocks;
+import com.computerstorage.common.registry.ModMenus;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 import com.mojang.logging.LogUtils;
 
@@ -15,6 +20,12 @@ public final class ComputerStorage {
 
     public ComputerStorage() {
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, ComputerStorageConfig.SPEC);
+
+        IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
+        ModBlocks.BLOCKS.register(modBus);
+        ModBlockEntities.BLOCK_ENTITIES.register(modBus);
+        ModMenus.MENUS.register(modBus);
+
         LOGGER.info("{} foundation bootstrapped", MOD_NAME);
     }
 
