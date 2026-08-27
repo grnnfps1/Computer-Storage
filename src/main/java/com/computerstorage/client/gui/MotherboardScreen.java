c736917c152e64acda39a7b5013d20003abe002c
grnnfps1 <contatogrn@icloud.com>
2026-08-27 08:22:44 -0300

    fix(gui): size motherboard screen for all inventories

--- src/main/java/com/computerstorage/client/gui/MotherboardScreen.java
+++ src/main/java/com/computerstorage/client/gui/MotherboardScreen.java
@@ -11,30 +11,27 @@ public final class MotherboardScreen extends AbstractContainerScreen<Motherboard
     public MotherboardScreen(MotherboardMenu menu, Inventory inventory, Component title) {
         super(menu, inventory, title);
         imageWidth = 176;
-        imageHeight = 190;
+        imageHeight = 208;
+        inventoryLabelY = 184;
     }
 
-    @Override
-    protected void renderBg(GuiGraphics graphics, float partialTick, int mouseX, int mouseY) {
-        int left = leftPos;
-        int top = topPos;
+    @Override protected void renderBg(GuiGraphics graphics, float partialTick, int mouseX, int mouseY) {
+        int left = leftPos, top = topPos;
         graphics.fill(left, top, left + imageWidth, top + imageHeight, 0xFF10151B);
         graphics.fill(left + 5, top + 5, left + imageWidth - 5, top + 28, 0xFF202A33);
-        graphics.fill(left + 5, top + 31, left + imageWidth - 5, top + 101, 0xFF161D24);
-        graphics.fill(left + 5, top + 104, left + imageWidth - 5, top + imageHeight - 5, 0xFF161D24);
-
+        graphics.fill(left + 5, top + 31, left + imageWidth - 5, top + 69, 0xFF161D24);
+        graphics.fill(left + 5, top + 72, left + imageWidth - 5, top + 120, 0xFF161D24);
+        graphics.fill(left + 5, top + 123, left + imageWidth - 5, top + imageHeight - 5, 0xFF161D24);
         graphics.drawString(font, Component.literal("COMPUTER STORAGE"), left + 10, top + 11, 0xFFFFFFFF, false);
         graphics.drawString(font, Component.literal("HARDWARE"), left + 8, top + 34, 0xFF8FA9BF, false);
-        graphics.drawString(font, Component.literal("INTERNAL STORAGE"), left + 8, top + 104, 0xFF8FA9BF, false);
+        graphics.drawString(font, Component.literal("INTERNAL STORAGE"), left + 8, top + 75, 0xFF8FA9BF, false);
     }
 
-    @Override
-    protected void renderLabels(GuiGraphics graphics, int mouseX, int mouseY) {
-        super.renderLabels(graphics, mouseX, mouseY);
+    @Override protected void renderLabels(GuiGraphics graphics, int mouseX, int mouseY) {
+        graphics.drawString(font, title, 8, 8, 0xFFFFFFFF, false);
         graphics.drawString(font, Component.literal("FE: " + energy()), 112, 11, 0xFFB9E6FF, false);
+        graphics.drawString(font, Component.literal("SYSTEM"), 8, 112, 0xFF8FA9BF, false);
     }
 
-    private int energy() {
-        return ((MotherboardControllerBlockEntity) menu.getController()).energyStored();
-    }
+    private int energy() { return menu.getController().energyStored(); }
 }
