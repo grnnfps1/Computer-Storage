7bb5743b6d2c9ae4172ff8b8b4dff63196529f14
grnnfps1 <contatogrn@icloud.com>
2026-08-27 10:42:32 -0300

    fix(client): register motherboard screen through client setup

--- src/main/java/com/computerstorage/client/ClientSetup.java
+++ src/main/java/com/computerstorage/client/ClientSetup.java
@@ -5,16 +5,16 @@ import com.computerstorage.client.gui.MotherboardScreen;
 import com.computerstorage.common.registry.ModMenus;
 import net.minecraft.client.gui.screens.MenuScreens;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.client.event.RegisterMenuScreensEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 @Mod.EventBusSubscriber(modid = ComputerStorage.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public final class ClientSetup {
     private ClientSetup() {}
 
     @SubscribeEvent
-    public static void registerScreens(RegisterMenuScreensEvent event) {
-        event.register(ModMenus.MOTHERBOARD.get(), MotherboardScreen::new);
+    public static void onClientSetup(FMLClientSetupEvent event) {
+        event.enqueueWork(() -> MenuScreens.register(ModMenus.MOTHERBOARD.get(), MotherboardScreen::new));
     }
 }
