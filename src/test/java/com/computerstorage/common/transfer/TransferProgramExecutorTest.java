7c7171d5353065b59b8a00aa2095d3f1164ad4f7
grnnfps1 <contatogrn@icloud.com>
2026-08-27 09:19:27 -0300

    test(logistics): verify multi-slot destination transfer

--- src/test/java/com/computerstorage/common/transfer/TransferProgramExecutorTest.java
+++ src/test/java/com/computerstorage/common/transfer/TransferProgramExecutorTest.java
@@ -25,4 +25,22 @@ class TransferProgramExecutorTest {
         assertEquals(16, destination.getStackInSlot(0).getCount());
         assertEquals(32, source.getStackInSlot(1).getCount());
     }
+
+    @Test
+    void fillsLaterDestinationSlotWhenFirstSlotCannotAccept() {
+        ItemStackHandler source = new ItemStackHandler(1);
+        source.setStackInSlot(0, new ItemStack(Items.IRON_INGOT, 16));
+        ItemStackHandler destination = new ItemStackHandler(3);
+        destination.setStackInSlot(0, new ItemStack(Items.COBBLESTONE, 64));
+
+        TransferProgram program = new TransferProgram("iron", "a", "b",
+                new TransferFilter(TransferFilter.Mode.WHITELIST, "minecraft:iron_ingot"),
+                100, 16, 1, 64, TransferCondition.ALWAYS, TransferSchedule.everyTick());
+
+        int moved = new TransferProgramExecutor().execute(program, source, destination);
+
+        assertEquals(16, moved);
+        assertEquals(16, destination.getStackInSlot(1).getCount());
+        assertEquals(0, source.getStackInSlot(0).getCount());
+    }
 }
